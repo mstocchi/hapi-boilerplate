10fc3cc9617f975913867e3c4449f03e0482f221
manolo.stocchi <manolo.stocchi@neomobile.com>
2017-10-22 16:34:17 +0200

    updates

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 'use strict';
 require('app-module-path/register');
 
+const settings = require('./lib/config');
 const Hapi = require('hapi');
 const Inert = require('inert')
 const Vision = require('vision')
 const HapiSwagger = require('hapi-swagger')
 const Good = require('good');
 const path = require('path');
-const authService = require('./lib/services/auth')
-const settings = require('./lib/config');
+const userService = require('lib/services/userService')
+
+const validateJwtToken = function (decoded, request, callback) {
+  
+  userService.findUserByEmailAndToken(decoded.userid, request.auth.token)
+  .then(res=>{
+    if(res.email === decoded.userid){
+      return callback(null, true)
+    }
+  })
+  .catch(err => {
+    console.log(err);
+    return callback(null, false)
+  })
+}
+
 
 const server = new Hapi.Server();
 
@@ -55,7 +70,7 @@ server.register([
     {
         register: require('./lib/plugins/auth-jwt'),
         options: {
-            validationHandler: authService.validateToken,
+            validationHandler: validateJwtToken,
             jwtencodekey: require('./lib/config').get('encryption.jwtsignkey')
         }
     },
